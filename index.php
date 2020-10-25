0c1e8a672af2eb3a993cf9d073e02971c556f4e5
laurencekeithabaring <72242203+laurencekeithabaring@users.noreply.github.com>
2020-10-26 01:01:37 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<style>
+body {background-color: powderblue;}
+.object{
+    background: #DFA995;
+    width: 300px;
+    height: 300px;
+    display:inline-block;
+    position: relative;
+	border: 2px solid black;
+}
+</style>
+<svg class ="object">
+  
+<svg version="1.1" id="cloud-animation" x="0px" y="0px" viewBox="0 0 350 200" enable-background="new 0 0 158.8 105.8" >
+	
+	
+	<path id="cloud" fill="white" stroke="#0000CD" stroke-width="10" stroke-miterlimit="20" d="M145,71.8c0.5-1.7,0.8-3.5,0.8-5.3c0-10.6-8.6-19.3-19.3-19.3c-2.6,0-5.1,0.5-7.3,1.5											c-3.7-11-12.4-19.7-23.4-23.4c-0.1,0-0.2-0.1-0.3-0.1C90.5,10.5,76.7,0,60.4,0C39.8,0,23.2,16.6,23.2,37.2c0,3.5,0.5,7,1.4,10.2
+	c4.6-1.5,9.5-1.5,13.8-0.2c-4.3-0.3-8.9,0.5-13.2,2.4c-1.1,0.5-2.1,1.1-3.1,1.7C16,55.6,12,62.7,11.7,70.7c3.2-0.8,6.5-0.7,9.6,0.2
+	c-3.8-0.1-7.7,0.6-11.3,2.5c-1.3,0.7-2.5,1.5-3.7,2.3c-3.6,3.1-5.9,7.8-5.9,12.9c0,9.5,7.7,17.2,17.2,17.2h1.6h122.3
+	c9.5,0,17.2-7.7,17.2-17.2C158.8,80.3,152.8,73.4,145,71.8z"/>
+	
+	
+	<animateMotion xlink:href="#cloud" dur="20s"	 begin="0s" repeatCount="indefinite" path="M59.5,38.9c21.3-2.9,22.5-31.1,73.3-10.7s10.7,48.7-16.4,57.4s-56.1,22.1-95.1-4.5S38.2,41.8,59.5,38.9z" />
+  
+  
+</svg>
+</svg>
